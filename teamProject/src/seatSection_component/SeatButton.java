e1af073ee7dc80ab9ff098ab522375ed6e66ba94
dltjr <dltjr@125.142.161.145>
2022-01-09 10:15:06 +0900

    중간저장

--- teamProject/src/seatSection_component/SeatButton.java
+++ teamProject/src/seatSection_component/SeatButton.java
@@ -2,17 +2,22 @@ package seatSection_component;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import teamProject.Start;
 
-public class SeatButton extends JButton {	
+public class SeatButton extends JButton {
 	
-	public SeatButton(int x, int y, String z) {
-		if(DB_Seats.seats_num_arr(z)) {
-			setText(z);
+	public SeatButton(int x, int y, String seat_number) {
+		
+		if(DB_Seats.seats_num_arr(seat_number)) {
+			setText(seat_number);
 			setLocation(x, y);
 			setSize(60, 60);		
 			setFont(new Font("MapoDPP", Font.PLAIN, 18));
@@ -20,9 +25,23 @@ public class SeatButton extends JButton {
 			setOpaque(true);
 			setBackground(new Color(0xdedede));
 			setHorizontalAlignment(JLabel.CENTER);
-			setVerticalAlignment(JLabel.CENTER);	
+			setVerticalAlignment(JLabel.CENTER);
+			
+			this.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					int answer = JOptionPane.showConfirmDialog(null, ""+seat_number+"번 자리를 사용하시겠습니까?", "confirm", JOptionPane.YES_NO_OPTION );
+					if(answer==JOptionPane.YES_OPTION) {
+						Start start = new Start();
+						// 초기화하면서 재시작 되는건 좋은데 전에게 꺼지지 않음
+						// 하나만 끄는 메소드로는 dispose가 있음
+						// setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 이런식으로 사용
+					}					
+				}
+			});
+			
 		} else {
-			setText(z);
+			setText(seat_number);
 			setLocation(x, y);
 			setSize(60, 60);		
 			setFont(new Font("MapoDPP", Font.PLAIN, 18));
